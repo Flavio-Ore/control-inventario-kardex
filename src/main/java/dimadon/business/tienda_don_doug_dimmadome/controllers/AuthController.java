1821c1f0f92e69fc8c9e947dc573e3f4ac0e6b78
Flavio Ore <101116042+Flavio-Ore@users.noreply.github.com>
2026-09-17 12:19:52 -0500

    feat: change domain cookie

--- src/main/java/dimadon/business/tienda_don_doug_dimmadome/controllers/AuthController.java
+++ src/main/java/dimadon/business/tienda_don_doug_dimmadome/controllers/AuthController.java
@@ -1,10 +1,13 @@
 package dimadon.business.tienda_don_doug_dimmadome.controllers;
 
+import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -17,7 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
 import dimadon.business.tienda_don_doug_dimmadome.entities.Usuario;
 import dimadon.business.tienda_don_doug_dimmadome.security.JwtTokenProvider;
 import dimadon.business.tienda_don_doug_dimmadome.services.ServiceUsuario;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
@@ -63,26 +65,24 @@ public class AuthController {
                     usuario.getTipoUsuario().getIdTipoUsuario());
 
             // Configurar la cookie para el access token
-            Cookie tokenCookie = new Cookie("token", token);
-            tokenCookie.setHttpOnly(false);
-            tokenCookie.setSecure(true); // Establecer en true si usas HTTPS
-            tokenCookie.setPath("/");
-            tokenCookie.setMaxAge(60 * 10); // 10 minutos
-            response.addCookie(tokenCookie);
+            // Cookie tokenCookie = new Cookie("token", token);
+            // tokenCookie.setHttpOnly(false);
+            // tokenCookie.setSecure(true); // Establecer en true si usas HTTPS
+            // tokenCookie.setPath("/");
+            // tokenCookie.setMaxAge(60 * 10); // 10 minutos
+            // response.addCookie(tokenCookie);
             // Building cookies
 
             // USING ResponseCookie
-            // String token = jwtTokenProvider.generateToken(email,
-            // usuario.getTipoUsuario().getIdTipoUsuario());
-            // ResponseCookie tokenCookie = ResponseCookie.from("token", token)
-            // .httpOnly(false)
-            // .secure(true)
-            // .sameSite("None")
-            // .path("/")
-            // .maxAge(Duration.ofMinutes(1))
-            // .build();
-
-            // response.addHeader(HttpHeaders.COOKIE, tokenCookie.toString());
+            ResponseCookie tokenCookie = ResponseCookie.from("token", token)
+                    .httpOnly(false)
+                    .secure(true)
+                    .sameSite("Lax")
+                    .domain("tienda-don-doug-dimmadome.vercel.app")
+                    .path("/")
+                    .maxAge(Duration.ofMinutes(10))
+                    .build();
+            response.addHeader(HttpHeaders.COOKIE, tokenCookie.toString());
 
             res.put("usuario", usuario);
             res.put("token", token);
